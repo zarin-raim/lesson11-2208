ad3d81506a45b72fd6da44d15d0f67f8580217c9
zarin-raim <zarinar1105@gmail.com>
2017-08-29 00:58:49 +0600

    istudent и student

---
+++ Practicalwork/IStudent.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Practicalwork
+{
+    interface IStudent
+    {
+        string Name { get; set; }
+        string FullName { get; set; }
+        int[] Grades { get; set; }
+        String GetName();
+        String GetFullName();
+        Double GetAvgGrade();
+    }
+}
---
+++ Practicalwork/Student.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Practicalwork
+{
+    class Student : IStudent
+    {
+        public string Name { get; set; }
+        public string FullName { get; set; }
+        public int[] Grades { get; set; }
+
+        public double GetAvgGrade()
+        {
+            int countGrades = Grades.Length;
+            double summa = 0;
+            foreach(var item in Grades)
+            {
+                summa += item;
+            }
+
+            return summa / countGrades;
+        }
+
+        public string GetFullName()
+        {
+            return FullName;
+        }
+
+        public string GetName()
+        {
+            return Name;
+        }
+    }
+}
